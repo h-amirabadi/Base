ee69456fdfbc72673d35930a7549e80020ea0cc5
h-amirabadi <h.amirabadi.n@gmail.com>
2019-02-03 09:48:47 +0330

    Spring Boot

---
+++ src/main/java/com/base/Application.java
@@ -0,0 +1,27 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/h")
+    public String hello() {
+        return "Request Mapping /h from Tomcat";
+    }
+
+}
